8e9d5bbb75c1419aa879fef51f9e91d558ed8c06
absak <70537969+absak@users.noreply.github.com>
2025-12-02 11:10:24 +0100

    update

--- mlops-demo/src/train.py
+++ mlops-demo/src/train.py
@@ -1,21 +1,20 @@
+# -*- coding: utf-8 -*-
 import mlflow
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 import joblib, os
-
 # Config MLflow
 mlflow.set_tracking_uri("mlruns")
 mlflow.set_experiment("mlops-demo")
 
 # Charger dataset
-df = pd.read_csv("mlops-demo/iris.csv")
+df = pd.read_csv("mlops-demo/data/iris.csv")
 X = df.drop("target", axis=1)
 y = df["target"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
@@ -27,5 +26,4 @@ with mlflow.start_run():
 
     mlflow.log_metric("f1_macro", f1)
     mlflow.log_artifact("mlops-demo/models/model.joblib")
-
 print("✅ Modèle entraîné et sauvegardé dans models/model.joblib")
