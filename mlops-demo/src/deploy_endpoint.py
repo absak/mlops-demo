347e3288396d706f41c4201655829be33c39a64e
absak <70537969+absak@users.noreply.github.com>
2025-12-12 14:35:41 +0100

    Add script to deploy SageMaker endpoint
    This script deploys a SageMaker endpoint for a model using SKLearn.

--- mlops-demo/src/deploy_endpoint.py
+++ mlops-demo/src/deploy_endpoint.py
@@ -1,1 +1,23 @@
 
+import os
+from sagemaker.sklearn.model import SKLearnModel
+
+role_arn = os.getenv("SAGEMAKER_ROLE_ARN")
+bucket = os.getenv("ARTIFACTS_BUCKET")
+prefix = "iris/artifacts"
+endpoint_name = "iris-rf-endpoint"
+
+model = SKLearnModel(
+    model_data=f"s3://{bucket}/{prefix}/model.joblib",
+    role=role_arn,
+    entry_point="src/predict.py",   # script d’inférence
+    framework_version="1.2-1"
+)
+
+predictor = model.deploy(
+    initial_instance_count=1,
+    instance_type="ml.t2.medium",
+    endpoint_name=endpoint_name
+)
+
+print("✅ Endpoint déployé :", endpoint_name)
